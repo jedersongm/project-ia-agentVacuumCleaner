07e21c3e715502d7e2e5a711795be11fad719f3a
jeder <jeder@LBVSZA-NBTI13.lan>
2019-03-16 21:50:33 -0300

    Classe Agent

---
+++ src/main/java/br/edu/fafic/bcc/ia/project/ia/agentvacuumcleaner/Agent.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package br.edu.fafic.bcc.ia.project.ia.agentvacuumcleaner;
+
+/**
+ *
+ * @author Jéderson Martins
+ * @email jedersongmoura@gmail.com
+ */
+public class Agent {
+
+    
+    public cleanner(){
+        
+    }
+}
