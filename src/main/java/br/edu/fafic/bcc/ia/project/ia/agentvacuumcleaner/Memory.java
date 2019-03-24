999a97f7a24182d607aa085ff9dff00b53925ba7
jeder <jeder@LBVSZA-NBTI13>
2019-03-24 15:25:28 -0300

    Modificações memory

--- src/main/java/br/edu/fafic/bcc/ia/project/ia/agentvacuumcleaner/Memory.java
+++ src/main/java/br/edu/fafic/bcc/ia/project/ia/agentvacuumcleaner/Memory.java
@@ -67,10 +67,18 @@ public class Memory {
     public boolean idIsCleanDesc(int id){
         int n = memory.size()-1;
         
-        while(memory.get(n).getAction() != 2){
-            if(memory.get(n).getAction() == 1 && memory.get(n).getPerception().equals(new Perception(id, false)))
-                return (true && idIsClean(id + 1));
-            n--;
+        if(noExistNoop()){
+            while(n!=0){
+                if(memory.get(n).getAction() == 1 && memory.get(n).getPerception().equals(new Perception(id, false)))
+                    return (true && idIsCleanDesc(id + 1));
+                n--;
+            } 
+        }else{
+            while(memory.get(n).getAction() != 2){
+                if(memory.get(n).getAction() == 1 && memory.get(n).getPerception().equals(new Perception(id, false)))
+                    return (true && idIsCleanDesc(id + 1));
+                n--;
+            }        
         }
         return false;
     }
